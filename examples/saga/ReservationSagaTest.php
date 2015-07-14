d40cf5e56bc831f8442b180b915a420e397bdfc3
Matthias Noback <matthiasnoback@gmail.com>
2015-07-14 10:02:01 +0200

    Add a saga example

---
+++ examples/saga/ReservationSagaTest.php
@@ -0,0 +1,61 @@
+<?php
+
+require __DIR__ . '/ReservationSaga.php';
+
+use Broadway\CommandHandling\CommandBusInterface;
+use Broadway\Saga\Testing\SagaScenarioTestCase;
+use Broadway\UuidGenerator\Testing\MockUuidSequenceGenerator;
+
+class ReservationSagaTest extends SagaScenarioTestCase
+{
+    protected function createSaga(CommandBusInterface $commandBus)
+    {
+        return new ReservationSaga($commandBus, new MockUuidSequenceGenerator(
+            array(
+                'bf142ea0-29f7-11e5-9d3f-0002a5d5c51b'
+            )
+        ));
+    }
+
+    /**
+     * @test
+     */
+    public function it_makes_a_seat_reservation_when_an_order_was_placed()
+    {
+        $this->scenario
+            ->when(new OrderPlaced('9d66f760-29f7-11e5-a239-0002a5d5c51b', 5))
+            ->then(array(
+                new MakeSeatReservation('bf142ea0-29f7-11e5-9d3f-0002a5d5c51b', 5)
+            ));
+    }
+
+    /**
+     * @test
+     */
+    public function it_marks_the_order_as_booked_when_the_seat_reservation_was_accepted()
+    {
+        $this->scenario
+            ->given(array(
+                new OrderPlaced('9d66f760-29f7-11e5-a239-0002a5d5c51b', 5)
+            ))
+            ->when(new ReservationAccepted('bf142ea0-29f7-11e5-9d3f-0002a5d5c51b'))
+            ->then(array(
+                new MarkOrderAsBooked('9d66f760-29f7-11e5-a239-0002a5d5c51b')
+            ));
+    }
+
+    /**
+     * @test
+     */
+    public function it_rejects_the_order_when_the_seat_reservation_was_rejected()
+    {
+        $this->scenario
+            ->given(array(
+                new OrderPlaced('9d66f760-29f7-11e5-a239-0002a5d5c51b', 5)
+            ))
+            ->when(new ReservationRejected('bf142ea0-29f7-11e5-9d3f-0002a5d5c51b'))
+            ->then(array(
+                new RejectOrder('9d66f760-29f7-11e5-a239-0002a5d5c51b')
+            ));
+    }
+}
