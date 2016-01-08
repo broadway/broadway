2a04c3f881b23069c70e264121fb8c73e9e06cfd
Alexander <iam.asm89@gmail.com>
2016-01-08 17:25:33 +0000

    [bundle] Add test showing that dbal is the default event store

--- test/Broadway/Bundle/BroadwayBundle/DependencyInjection/BroadwayExtensionTest.php
+++ test/Broadway/Bundle/BroadwayBundle/DependencyInjection/BroadwayExtensionTest.php
@@ -187,6 +187,23 @@ class BroadwayExtensionTest extends ExtensionTestCase
         $this->assertFalse($this->container->hasDefinition('broadway.auditing.command_logger'));
     }
 
+    /**
+     * @test
+     */
+    public function it_has_dbal_as_default_event_store()
+    {
+        $this->load($this->extension, array());
+
+        $this->assertTrue(
+            $this->container->hasDefinition('broadway.event_store.dbal')
+        );
+        $this->assertTrue($this->container->hasAlias('broadway.event_store'));
+        $this->assertEquals(
+            'broadway.event_store.dbal',
+            $this->container->getAlias('broadway.event_store')
+        );
+    }
+
     private function assertDICAliasClass($aliasId, $class)
     {
         $definitionId = (string) $this->container->getAlias($aliasId);
