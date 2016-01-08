3515d7f3aaae71d41247e93243890301cee56182
Alexander <iam.asm89@gmail.com>
2016-01-08 17:40:12 +0000

    [bundle] Silence dbal compiler pass if dbal is not used
    The dbal event store can be disabled in the extension. In that case the
    connection name will not be configured at all. If it's not configured
    there is nothing to check so the compiler pass that checks the
    connection name should be silent.

--- test/Broadway/Bundle/BroadwayBundle/DependencyInjection/DefineDBALEventStoreConnectionCompilerPassTest.php
+++ test/Broadway/Bundle/BroadwayBundle/DependencyInjection/DefineDBALEventStoreConnectionCompilerPassTest.php
@@ -23,13 +23,26 @@ class DefineDBALEventStoreConnectionCompilerPassTest extends PHPUnit_Framework_T
 
         $this->container = $this->getMockBuilder('Symfony\Component\DependencyInjection\ContainerBuilder')
             ->disableOriginalConstructor()
-            ->setMethods(array('hasDefinition', 'setAlias', 'getParameter'))
+            ->setMethods(array('hasDefinition', 'setAlias', 'hasParameter', 'getParameter'))
             ->getMock();
-        
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_nothing_if_dbal_was_not_configured()
+    {
         $this->container
-            ->method('getParameter')
+            ->method('hasParameter')
             ->with('broadway.event_store.dbal.connection')
-            ->will($this->returnValue('default'));
+            ->will($this->returnValue(false));
+
+        $this->container
+            ->method('hasDefinition')
+            ->with('doctrine.dbal.default_connection')
+            ->will($this->returnValue(false)); // should not matter
+
+        $this->compilerPass->process($this->container);
     }
 
     /**
@@ -37,6 +50,7 @@ class DefineDBALEventStoreConnectionCompilerPassTest extends PHPUnit_Framework_T
      */
     public function it_aliases_the_doctrine_connection()
     {
+        $this->mockConnectionParameter();
         $this->container
             ->method('hasDefinition')
             ->with('doctrine.dbal.default_connection')
@@ -57,10 +71,24 @@ class DefineDBALEventStoreConnectionCompilerPassTest extends PHPUnit_Framework_T
      */
     public function it_throws_an_error_for_invalid_doctrine_connection()
     {
+        $this->mockConnectionParameter();
         $this->container
             ->method('hasDefinition')
             ->will($this->returnValue(false));
 
         $this->compilerPass->process($this->container);
+    }
+
+    private function mockConnectionParameter() {
+        $this->container
+            ->method('hasParameter')
+            ->with('broadway.event_store.dbal.connection')
+            ->will($this->returnValue(true));
+
+        $this->container
+            ->method('getParameter')
+            ->with('broadway.event_store.dbal.connection')
+            ->will($this->returnValue('default'));
+
     }
 }
