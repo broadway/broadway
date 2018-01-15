8c1a063b6769ed692c705f54fd4b675fc9553420
othillo <othillo@othillo.nl>
2018-01-15 10:43:23 +0100

    enforce license

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the broadway/broadway package.
+ *
+ * (c) Qandidate.com <opensource@qandidate.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 if (file_exists($file = __DIR__.'/../vendor/autoload.php')) {
--- examples/event-sourced-domain-with-tests/InvitationCommandHandlerTest.php
+++ examples/event-sourced-domain-with-tests/InvitationCommandHandlerTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the broadway/broadway package.
+ *
+ * (c) Qandidate.com <opensource@qandidate.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 use Broadway\CommandHandling\CommandHandler;
