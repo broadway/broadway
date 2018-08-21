8f9912ce9a602edae72a975a45a1cca469bc1a8f
Willem-Jan <wjzijderveld@gmail.com>
2018-08-21 12:00:50 +0200

    Added exampel for querying the event store

---
+++ examples/querying-the-event-store/query-memory-events-with-criteria.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types=1);
+
+use Broadway\Domain\DomainEventStream;
+use Broadway\EventStore\InMemoryEventStore;
+use Broadway\EventStore\Management\Criteria;
+use Broadway\EventStore\CallableEventVisitor;
+
+require __DIR__ . '/../event-sourced-domain-with-tests/Invites.php';
+
+$logger = new StdoutLogger();
+$store = new InMemoryEventStore();
+
+// Prepare some invites
+$staleInvited = Invitation::invite('061e5360-ff48-4468-a672-e49ed77e0fc2', 'Barry');
+$acceptedInvite = Invitation::invite('590ae831-b854-40e6-bcc2-ca7d9d552421', 'Mary');
+$acceptedInvite->accept();
+$declinedInvite = Invitation::invite('caae5d47-5ee7-4821-9f21-a2b65b373438', 'Larry');
+$declinedInvite->decline();
+
+// Store the events
+$staleInvitationEvents = $staleInvited->getUncommittedEvents();
+$acceptedInviteEvents = $acceptedInvite->getUncommittedEvents();
+$declinedInviteEvents = $declinedInvite->getUncommittedEvents();
+$store->append($staleInvited->getAggregateRootId(), $staleInvitationEvents);
+$store->append($acceptedInvite->getAggregateRootId(), $acceptedInviteEvents);
+$store->append($declinedInvite->getAggregateRootId(), $declinedInviteEvents);
+
+$allMessages = new DomainEventStream(array_merge(
+    $staleInvitationEvents->getIterator()->getArrayCopy(),
+    $acceptedInviteEvents->getIterator()->getArrayCopy(),
+    $declinedInviteEvents->getIterator()->getArrayCopy()
+));
+
+// Create the criteria to retrieve AcceptedEvent events
+$acceptedInvitesCriteria = Criteria::create()->withEventTypes([strtr(AcceptedEvent::class, '\\', '.')]);
+
+// Query the event store with the criteria and fill an array with ids of accepted invites
+$acceptedEventsReadModel = [];
+$store->visitEvents($acceptedInvitesCriteria, new CallableEventVisitor(function ($domainMessage) use (&$acceptedEventsReadModel) {
+    $acceptedEventsReadModel[] = ['inviteId' => $domainMessage->getPayload()->invitationId];
+}));
+
+$logger->info('Accepted invites', $acceptedEventsReadModel);
+
+
+// For more complex cases you would need more complex queries.
+// The Criteria will only deal with simple use cases.
+// If you need more complex queries you could do something like this
+// Note: the example is with the InMemoryStore, but you can use the same idea with complex SQL queries for example: 
+// SELECT a.uuid FROM events a LEFT JOIN events b ON a.uuid = b.uuid AND b.type IN ('AcceptedEvent', 'DeclinedEvent') WHERE a.`type` = 'InvitedEvent' AND b.uuid IS NULL;
+
+$staleInvites = [];
+foreach ($allMessages as $domainMessage) {
+    switch ($domainMessage->getType()) {
+        case 'InvitedEvent':
+            $staleInvites[(string) $domainMessage->getId()] = true;
+            break;
+        case 'AcceptedEvent':
+        case 'DeclinedEvent':
+            unset($staleInvites[(string) $domainMessage->getId()]);
+            break;
+    }
+}
+
+$logger->info('Stale invites', $staleInvites);
