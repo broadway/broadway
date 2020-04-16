7deb03d934eac01113095ff33329fac272b208e4
othillo <othillo@othillo.nl>
2020-04-16 22:54:50 +0200

    simple event replayer

---
+++ src/Broadway/Replaying/Replayer.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Broadway\Replaying;
+
+use Broadway\EventStore\EventVisitor;
+use Broadway\EventStore\Management\Criteria;
+use Broadway\EventStore\Management\EventStoreManagement;
+
+abstract class Replayer
+{
+    private $eventStore;
+    private $eventVisitor;
+
+    public function __construct(
+        EventStoreManagement $eventStore,
+        EventVisitor $eventVisitor
+    ) {
+        $this->eventStore = $eventStore;
+        $this->eventVisitor = $eventVisitor;
+    }
+
+    public function replay(Criteria $criteria)
+    {
+        $this->eventStore->visitEvents($criteria, $this->eventVisitor);
+    }
+}
